5e06f023f223a9740f7c22a63399695265c1fdcb
Gaétan J.A.M. Jalin <gaetanjalin@gmail.com>
2024-11-15 23:47:26 +0100

    Domain validation

--- ncm/src/domain.cpp
+++ ncm/src/domain.cpp
@@ -4,6 +4,8 @@
 #include <cstdio>
 #include <cmath>
 
+#include <map>
+#include <vector>
 #include <fstream>
 #include <tuple>
 #include <algorithm>
@@ -46,6 +48,104 @@ namespace
             throw invalid_file_expression(loc);
 
         return {name, {.v1 = v1, .v2 = v2, .type = type}};
+    }
+
+    template<typename Key, typename Value, typename Predicate>
+    bool compare_any_of(std::map<Key, Value> const& m, Predicate p)
+    {
+        for (auto it = m.cbegin(); it != m.cend(); ++it)
+            for (auto it2 = std::next(it); it2 != m.cend(); ++it2)
+                if (p(*it, *it2))
+                    return true;
+
+        return false;
+    }
+
+    void check_duplicate_vertices(std::map<std::string, vertex_t> const& vertices)
+    {
+        std::string k1, k2;
+        bool has_duplicate = compare_any_of(vertices,
+        [&k1, &k2](auto const& v1, auto const& v2) -> bool
+        {
+            float dx = v1.second.x - v2.second.x;
+            float dy = v1.second.y - v2.second.y;
+            bool are_same = (std::sqrt(dx*dx + dy*dy)) < SMALL_DX;
+            if (are_same)
+            {
+                k1 = v1.first;
+                k2 = v2.first;
+            }
+
+            return are_same;
+        });
+
+        if (has_duplicate)
+            throw std::logic_error("Vertices have similar coordinates: " + k1 +  "," + k2);
+    }
+
+    void check_boundaries_valid(std::map<std::string, vertex_t> const& vertices,
+                                std::map<std::string, boundary_t> const& boundaries)
+    {
+        for (auto const& [name, boundary] : boundaries)
+        {
+            if (vertices.find(boundary.v1) == vertices.end())
+                throw std::logic_error("Undefined vertex " + boundary.v1 + " for boundary " + name + "!");
+            if (vertices.find(boundary.v2) == vertices.end())
+                throw std::logic_error("Undefined vertex " + boundary.v2 + " for boundary " + name + "!");
+        }
+    }
+
+    void check_duplicate_boundaries(std::map<std::string, boundary_t> const& boundaries)
+    {
+        std::string k1, k2;
+        bool has_duplicate = compare_any_of(boundaries,
+        [&k1, &k2](auto const& b1, auto const& b2) -> bool
+        {
+            bool are_same = (b1.second.v1 == b2.second.v1 && b1.second.v2 == b2.second.v2) ||
+                            (b1.second.v1 == b2.second.v2 && b1.second.v2 == b2.second.v1);
+            if (are_same)
+            {
+                k1 = b1.first;
+                k2 = b2.first;
+            }
+
+            return are_same;
+        });
+
+        if (has_duplicate)
+            throw std::logic_error("Boundaries have same end points: " + k1 + "," + k2);
+    }
+
+    void check_zero_length_boundary(std::map<std::string, boundary_t> const& boundaries)
+    {
+        std::string k;
+        bool has_null_boundary = std::any_of(boundaries.begin(), boundaries.end(),
+        [&k](auto const& b) -> bool
+        {
+            bool is_null = b.second.v1 == b.second.v2;
+            if (is_null)
+                k = b.first;
+
+            return is_null;
+        });
+
+        if (has_null_boundary)
+            throw std::logic_error("Boundary has zero-length: " + k);
+    }
+
+    void check_boundaries_are_closed(std::map<std::string, boundary_t> const& boundaries)
+    {
+        std::map<std::string, std::vector<std::string>> connections;
+
+        for (auto [key, boundary] : boundaries)
+        {
+            connections[boundary.v1].push_back(key);
+            connections[boundary.v2].push_back(key);
+        }
+
+        for (auto [key, links] : connections)
+            if (links.size() == 1)
+                throw std::logic_error("Dangling connection: vertex " + key + " on boundary " + links.front());
     }
 }
 
@@ -97,23 +197,16 @@ domain_t read_domain_file(std::string const& filename)
 void validate_domain(domain_t const& domain)
 {
     // Basic logic checks
-    if (domain.vertices.size() < 3) throw std::logic_error("Domain vertices do not form a polygonal shape!");
-    if (domain.boundaries.size() < 3) throw std::logic_error("Domain boundaries do not form a polygonal shape!");
+    if (domain.vertices.size() < 3) throw std::logic_error("Too few vertice!");
+    if (domain.boundaries.size() < 3) throw std::logic_error("Too few boundaries!");
 
-    // Check that no two vertices have same coordinates (duplicate) or is too close to another
-    auto are_vertex_too_close = [](vertex_t const& v1, vertex_t const& v2) -> bool
-    {
-        return (std::sqrt(v1.x*v2.x + v1.y*v2.y)) < SMALL_DX;
-    };
+    check_boundaries_valid(domain.vertices, domain.boundaries);
 
-    for (auto it = domain.vertices.cbegin(); it != domain.vertices.cend(); ++it)
-        for (auto it2 = std::next(it); it2 != domain.vertices.cend(); ++it2)
-            if (are_vertex_too_close(it->second, it2->second))
-                throw std::logic_error("");
-
-    // Check that no boundaries are duplicate (same end points)
-    // Check that no boundary has zero length/starts and ends on the same vertex
+    check_duplicate_vertices(domain.vertices);
+    check_duplicate_boundaries(domain.boundaries);
+    check_zero_length_boundary(domain.boundaries);
     // Check that every boundary is connected to another on both ends
     // Check that all connected boundary form a closed shape (implicit from previous check?)
+    check_boundaries_are_closed(domain.boundaries);
 }
 
